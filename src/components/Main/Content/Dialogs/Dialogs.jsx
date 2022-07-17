ffb3bc4db52e450f23e5a42d9e77b2a4498908b7
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-07-17 17:36:07 +0300

    markupped dialogs page

--- src/components/Main/Content/Dialogs/Dialogs.jsx
+++ src/components/Main/Content/Dialogs/Dialogs.jsx
@@ -3,7 +3,38 @@ import classes from './Dialogs.module.css'
 const Dialogs = () => {
   return (
     <div className={classes.dialogs}>
-      <h2>Dialogs</h2>
+      <h2 className={classes.h2}>Dialogs</h2>
+      <div className={classes.convs}>
+        <div className={classes.conv}>
+          Dimych
+        </div>
+        <div className={classes.conv}>
+          Sveta
+        </div>
+        <div className={classes.conv}>
+          Viktor
+        </div>
+        <div className={classes.conv}>
+          Tanya
+        </div>
+        <div className={classes.conv}>
+          Luciano
+        </div>
+        <div className={classes.conv}>
+          Daniella
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.message}>
+          Hello
+        </div>
+        <div className={classes.message}>
+          Hi
+        </div>
+        <div className={classes.message}>
+          Hi-hi
+        </div>
+      </div>
     </div>
   );
 }
